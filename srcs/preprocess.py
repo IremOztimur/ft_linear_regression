abde26bc7267c31aec58ef61a4a233b8581ba2ac
IremOztimur <iremoztimur07@gmail.com>
2025-01-20 19:40:29 +0300

    [ADD] standard scaler and train test split

---
+++ srcs/preprocess.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+class CustomStandardScaler:
+    def fit(self, X):
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+        return self
+    
+    def transform(self, X):
+        return (X - self.mean) / self.std
+    
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
+    
+    
+def train_test_split(X, y, test_size=0.2, random_state=None):
+    if random_state:
+        np.random.seed(random_state)
+        
+    indices = np.random.permutation(X.shape[0])
+    test_size = int(X.shape[0] * test_size)
+    test_indices = indices[:test_size]
+    train_indices = indices[test_size:]
+    
+    return X[train_indices], X[test_indices], y[train_indices], y[test_indices]
