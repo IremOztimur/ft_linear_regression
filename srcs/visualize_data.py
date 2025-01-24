a6023430b8dac53b4426097e0c3ccc26399d42a3
IremOztimur <iremoztimur07@gmail.com>
2025-01-24 11:51:24 +0300

    [UPDATE] scaler import path

--- srcs/visualize_data.py
+++ srcs/visualize_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from preprocess import CustomStandardScaler
+from model.preprocess import CustomStandardScaler
 
 df = pd.read_csv('../data/data.csv')
 
