df5d70099da8c83ac705e4965244eb877c8ef495
IremOztimur <iremoztimur07@gmail.com>
2025-01-24 11:50:50 +0300

    [ADD] prediction loop with quit option

--- srcs/predict.py
+++ srcs/predict.py
@@ -1,7 +1,7 @@
 import argparse
 import pyfiglet
 from model.regression import LinearRegression
-from preprocess import CustomStandardScaler
+from model.preprocess import CustomStandardScaler
 from model.layer import Dense
 import numpy as np
 
@@ -19,17 +19,23 @@ def main():
 
     word = pyfiglet.figlet_format("Linear Regression")
     print(f"\033[95m{word}\033[00m")
-    mileage = float(input("Enter the mileage value: "))
-
     
-    X_scaled = scaler.transform(np.array([[mileage]]))
-
-    y_pred = model.predict(X_scaled)
-    y_pred_org = y_pred * model.std['price'] + model.mean['price']
-
-    print(f"\033[95mPredicted Price: {y_pred_org[0][0]:.2f}\033[00m")
+    while True:
+        mileage = input("Enter the mileage value (or type 'quit' to exit): ")
+        if mileage.lower() == 'quit':
+            break
+        
+        try:
+            mileage = float(mileage)
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+            continue
+        
+        X_scaled = scaler.transform(np.array([[mileage]]))
+        y_pred = model.predict(X_scaled)
+        y_pred_org = y_pred * model.std['price'] + model.mean['price']
+        
+        print(f"\033[95mPredicted Price: {y_pred_org[0][0]:.2f}\033[00m")
     
-    
-
 if __name__ == "__main__":
     main()
